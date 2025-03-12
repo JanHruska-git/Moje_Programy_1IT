86913cc0864c8d0972db808539f0b5043240987e
Jan Hruška 1IT <slimeop@seznam.cz>
2025-03-12 22:56:00 +0100

    Add files via upload

---
+++ Jednoduché_počítání_kalorií.py
@@ -0,0 +1,99 @@
+soucer = 0
+while True:
+    otazka = input("Zadej kolik chceš spočítat jídel (2-10): ")
+    
+    if otazka == '2':
+        kalorie1 = float(input("Zadej kalorie prvního jídla: "))
+        kalorie2 = float(input("Zadej kalorie druhého jídla: "))
+        soucet = kalorie1 + kalorie2
+        print(f"Součet všech jídel je {soucet}")
+        
+    elif otazka == '3':
+        kalorie1 = float(input("Zadej kalorie prvního jídla: "))
+        kalorie2 = float(input("Zadej kalorie druhého jídla: "))
+        kalorie3 = float(input("Zadej kalorie třetího jídla: "))
+        soucet = kalorie1 + kalorie2 + kalorie3
+        print(f"Součet všech jídel je {soucet}")
+        
+    elif otazka == '4':
+        kalorie1 = float(input("Zadej kalorie prvního jídla: "))
+        kalorie2 = float(input("Zadej kalorie druhého jídla: "))
+        kalorie3 = float(input("Zadej kalorie třetího jídla: "))
+        kalorie4 = float(input("Zadej kalorie čtvrtého jídla: "))
+        soucet = kalorie1 + kalorie2 + kalorie3 + kalorie4
+        print(f"Součet všech jídel je {soucet}")
+        
+    elif otazka == '5':
+        kalorie1 = float(input("Zadej kalorie prvního jídla: "))
+        kalorie2 = float(input("Zadej kalorie druhého jídla: "))
+        kalorie3 = float(input("Zadej kalorie třetího jídla: "))
+        kalorie4 = float(input("Zadej kalorie čtvrtého jídla: "))
+        kalorie5 = float(input("Zadej kalorie pátého jídla: "))
+        soucet = kalorie1 + kalorie2 + kalorie3 + kalorie4 + kalorie5
+        print(f"Součet všech jídel je {soucet}")
+        
+    elif otazka == '6':
+        kalorie1 = float(input("Zadej kalorie prvního jídla: "))
+        kalorie2 = float(input("Zadej kalorie druhého jídla: "))
+        kalorie3 = float(input("Zadej kalorie třetího jídla: "))
+        kalorie4 = float(input("Zadej kalorie čtvrtého jídla: "))
+        kalorie5 = float(input("Zadej kalorie pátého jídla: "))
+        kalorie6 = float(input("Zadej kalorie šestého jídla: "))
+        soucet = kalorie1 + kalorie2 + kalorie3 + kalorie4 + kalorie5 + kalorie6
+        print(f"Součet všech jídel je {soucet}")
+        
+    elif otazka == '7':
+        kalorie1 = float(input("Zadej kalorie prvního jídla: "))
+        kalorie2 = float(input("Zadej kalorie druhého jídla: "))
+        kalorie3 = float(input("Zadej kalorie třetího jídla: "))
+        kalorie4 = float(input("Zadej kalorie čtvrtého jídla: "))
+        kalorie5 = float(input("Zadej kalorie pátého jídla: "))
+        kalorie6 = float(input("Zadej kalorie šestého jídla: "))
+        kalorie7 = float(input("Zadej kalorie sedmého jídla: "))
+        soucet = kalorie1 + kalorie2 + kalorie3 + kalorie4 + kalorie5 + kalorie6 + kalorie7
+        print(f"Součet všech jídel je {soucet}")
+        
+    elif otazka == '8':
+        kalorie1 = float(input("Zadej kalorie prvního jídla: "))
+        kalorie2 = float(input("Zadej kalorie druhého jídla: "))
+        kalorie3 = float(input("Zadej kalorie třetího jídla: "))
+        kalorie4 = float(input("Zadej kalorie čtvrtého jídla: "))
+        kalorie5 = float(input("Zadej kalorie pátého jídla: "))
+        kalorie6 = float(input("Zadej kalorie šestého jídla: "))
+        kalorie7 = float(input("Zadej kalorie sedmého jídla: "))
+        kalorie8 = float(input("Zadej kalorie osmého jídla: "))
+        soucet = kalorie1 + kalorie2 + kalorie3 + kalorie4 + kalorie5 + kalorie6 + kalorie7 + kalorie8
+        print(f"Součet všech jídel je {soucet}")
+        
+    elif otazka == '9':
+        kalorie1 = float(input("Zadej kalorie prvního jídla: "))
+        kalorie2 = float(input("Zadej kalorie druhého jídla: "))
+        kalorie3 = float(input("Zadej kalorie třetího jídla: "))
+        kalorie4 = float(input("Zadej kalorie čtvrtého jídla: "))
+        kalorie5 = float(input("Zadej kalorie pátého jídla: "))
+        kalorie6 = float(input("Zadej kalorie šestého jídla: "))
+        kalorie7 = float(input("Zadej kalorie sedmého jídla: "))
+        kalorie8 = float(input("Zadej kalorie osmého jídla: "))
+        kalorie9 = float(input("Zadej kalorie devátého jídla: "))
+        soucet = kalorie1 + kalorie2 + kalorie3 + kalorie4 + kalorie5 + kalorie6 + kalorie7 + kalorie8 + kalorie9
+        print(f"Součet všech jídel je {soucet}")
+        
+    elif otazka == '10':
+        kalorie1 = float(input("Zadej kalorie prvního jídla: "))
+        kalorie2 = float(input("Zadej kalorie druhého jídla: "))
+        kalorie3 = float(input("Zadej kalorie třetího jídla: "))
+        kalorie4 = float(input("Zadej kalorie čtvrtého jídla: "))
+        kalorie5 = float(input("Zadej kalorie pátého jídla: "))
+        kalorie6 = float(input("Zadej kalorie šestého jídla: "))
+        kalorie7 = float(input("Zadej kalorie sedmého jídla: "))
+        kalorie8 = float(input("Zadej kalorie osmého jídla: "))
+        kalorie9 = float(input("Zadej kalorie devátého jídla: "))
+        kalorie10 = float(input("Zadej kalorie desátého jídla: "))
+        soucet = kalorie1 + kalorie2 + kalorie3 + kalorie4 + kalorie5 + kalorie6 + kalorie7 + kalorie8 + kalorie9 + kalorie10
+        print(f"Součet všech jídel je {soucet}")
+
+		
+	
+		
+	
+

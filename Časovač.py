823b56c1cd9d5f089b60dbfa22caddad2a00bbfd
Jan Hruška 1IT <slimeop@seznam.cz>
2025-03-13 14:27:42 +0100

    Add files via upload

---
+++ Časovač.py
@@ -0,0 +1,14 @@
+import time
+cas = int(input("Do jakeho cisla chces, aby casovac pocital: "))
+start = input("jeslti chces, aby zacl napis start: ")
+while True:
+	if start == "start" :
+		for i in range(0, cas):
+			print(i + 1)
+			time.sleep(1)
+	if i + 1 == cas :
+			print("Konec")
+			break
+			
+
+	

b038008f95ff720c0fde56728f887ee32895d1be
Jan Hruška 1IT <slimeop@seznam.cz>
2025-05-20 10:46:23 +0200

    Sejf.py

--- Sejf.py
+++ Sejf.py
@@ -3,7 +3,10 @@ for a in (2, 4, 6, 8):
     for b in range(1, 10):
         for c in range(1,10):
             for d in range(1,5):
-                if a + b + c + d == 21:
-                    print(a,b,c,d)
-                    count += 1
+                if a!=b:
+                    if b!=c:
+                        if c!=d:
+                            if a + b + c + d == 21:
+                                print(a,b,c,d)
+                                count += 1
 print(f"Pocet cifer: {count}")
